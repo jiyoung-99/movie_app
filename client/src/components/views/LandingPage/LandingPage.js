e314cb1a88e8230fc395a7e7724a1fbd1281ae40
jiyoung-99 <1skylar1111@gmail.com>
2021-03-22 21:20:42 +0900

    안나옴

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
+import MainImage from './Sections/MainImage';
 
 function LandingPage() {
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    //밑에 MainMovieImage 
+    console.log('MainMovieImage', MainMovieImage)
+
+    useEffect(() => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        fetchMovies(endpoint)
+    }, [])
+
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint)
+        .then(response => response.json())
+        .then(response => {
+            console.log([response.results[0]])
+            setMovies([...Movies, ...response.results])
+            setMainMovieImage([response.results[0]])
+        })
+    }
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{width: '100%', margin: '0'}}>
+            {/* Main Image */}
+            {MainMovieImage &&
+                <MainImage
+                    
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview}
+                />
+            }
+            <div style={{width:'85%', margin: '1rem auto'}}>
+                <h2>Movies by latest</h2>
+                <hr/>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+            <div style={{display: 'flex', justifyContent:'center'}}>
+                <button>Load More</button>
+            </div>
+        </div>
     )
+    
+
 }
 
 export default LandingPage
